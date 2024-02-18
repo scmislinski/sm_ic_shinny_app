08a70ef8ee26eaef000983df97810ac09a29f6c8
scmislinski <scmislinski@bren.ucsb.edu>
2024-02-17 19:16:46 -0800

    I made a few pages. I haven't changed the theme yet.

---
+++ Scripts/sm_ic_shinny_app_week_6_checkin.R
@@ -0,0 +1,97 @@
+#### Shiny App Basic set up
+
+# 2/9/24
+
+# Libraries
+library(shiny)
+library(tidyverse)
+library(here)
+library(tmap)
+library(broom)
+library(sf)
+library(lubridate)
+library(tsibble)
+library(feasts)
+library(fable)
+
+############# USER INTERFACE ########################################
+
+ui <- fluidPage(
+  titlePanel("Land Use Changes and Fires for the Islands of Hawai'i"),
+  tabsetPanel(
+
+    tabPanel(
+      title = 'Introduction',
+      p('Introduction text')
+    ), ############################################# END OF TAB 1
+
+    tabPanel(
+      title = "Fires Map",
+      fluidPage(
+        fluidRow(
+          column(width = 4,
+                 h3('Choices for inputs to the map: By year, by Ahupua`a (watershed)'),
+                 ), #End of column 1 tab 2
+          column(width = 8,
+                 h3('Fires map (tmap)'),
+          ) #End of column 2 tab 2
+        )#End of fluid row tab 2
+      ) #End of fluid page tab 2
+    ), #################################### END OF TAB 2
+
+    tabPanel(
+      title = "Seasonallity",
+      fluidPage(
+        fluidRow(
+          column(width = 4,
+                 h3('Widget select box to select which year to highlight'),
+          ), #End of column 1 tab 3
+          column(width = 8,
+                 h3('Seasonal plots of past fires'),
+          ) #End of column 2 tab 3
+        )#End of fluid row tab 3
+      ) #End of fluid page tab 3
+    ), ################################## END OF TAB 3
+
+    tabPanel(
+      title = "Tab 4",
+      fluidPage(
+        fluidRow(
+          column(width = 4,
+                 h3('column 1 header'),
+          ), #End of column 1 tab 4
+          column(width = 8,
+                 h3('column 2 header'),
+          ) #End of column 2 tab 4
+        )#End of fluid row tab 4
+      ) #End of fluid page tab 4
+    ), ################################### END OF TAB 4
+
+    tabPanel(
+      title = "Citations",
+      p('Citations text')
+    ), ####################################### END OF TAB 5
+  ), #end of tabsetPanel
+)### End of fluidPage function
+
+
+
+
+############ SERVER FUNCTION #########################################
+
+server <- function(input, output) { #plot 1
+}
+
+
+### The following line of code makes this into a shiny app
+shinyApp(ui = ui, server = server)
+
+
+
+
+
+
+
+
+
+
