dfdb1105c17c326d0823698d6950559db441766a
scmislinski <scmislinski@bren.ucsb.edu>
2024-02-18 14:44:44 -0800

    New theme

--- Scripts/sm_ic_shinny_app_week_6_checkin.R
+++ Scripts/sm_ic_shinny_app_week_6_checkin.R
@@ -13,31 +13,35 @@ library(lubridate)
 library(tsibble)
 library(feasts)
 library(fable)
+library(bslib)
 
 ############# USER INTERFACE ########################################
 
 ui <- fluidPage(
+  theme = bs_theme(bootswatch = 'spacelab'),
   titlePanel("Land Use Changes and Fires for the Islands of Hawai'i"),
   tabsetPanel(
 
     tabPanel(
       title = 'Introduction',
       p('Introduction text')
-    ), ############################################# END OF TAB 1
+    ), ################################################ END OF TAB 1
 
     tabPanel(
       title = "Fires Map",
       fluidPage(
         fluidRow(
           column(width = 4,
-                 h3('Choices for inputs to the map: By year, by Ahupua`a (watershed)'),
+                 selectInput("select", label = h3("Choose to Display on graph"),
+                             choices = list("Year" = 1, "Island" = 2, "Ahupua'a (Watershed)" = 3),
+                             selected = 1),
                  ), #End of column 1 tab 2
           column(width = 8,
                  h3('Fires map (tmap)'),
           ) #End of column 2 tab 2
         )#End of fluid row tab 2
       ) #End of fluid page tab 2
-    ), #################################### END OF TAB 2
+    ), ####################################################### END OF TAB 2
 
     tabPanel(
       title = "Seasonallity",
